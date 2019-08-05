47e89f472466b1c376b6f2e8edc76e43b34ff649
Vitalii Elenhaupt <velenhaupt@gmail.com>
2019-08-05 10:55:49 +0300

    Create EditorPane

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import cx from 'classnames';
 
 import payloadSample from './PayloadSample';
-import JSONEditor from './components/JSONEditor';
+import EditorPane from './components/EditorPane';
 import Viewer from './components/Viewer';
-import Toolbar from './components/Toolbar';
 
-import { safeJSONParse, formattedJSON } from './utils/json';
+import { safeJSONParse } from './utils/json';
 import hashCode from './utils/string';
 
 import './App.css';
@@ -17,49 +15,33 @@ export default class App extends React.Component {
 
     let payload = payloadSample;
     let object = safeJSONParse(payloadSample);
-    this.state = { payload, object, autoformat: true };
+    this.state = { payload, object };
 
     this.onPayloadChanged = this.onPayloadChanged.bind(this);
-    this.onAutoformattingToggle = this.onAutoformattingToggle.bind(this);
   }
 
-  onPayloadChanged(payload, autoformat = this.state.autoformat) {
+  onPayloadChanged(payload) {
     let object = safeJSONParse(payload);
-    if (object && autoformat) payload = formattedJSON(object);
-    this.setState({ payload, object, autoformat });
-  }
-
-  onAutoformattingToggle(event) {
-    let autoformat = event.target.checked;
-    this.onPayloadChanged(this.state.payload, autoformat);
+    this.setState({ payload, object });
   }
 
   render() {
-    let { object } = this.state;
+    let { object, payload } = this.state;
 
     return(
       <div className="App">
-        <div className="splitpane">
-          <Toolbar>
-            <input
-              className="autoformat"
-              type="checkbox"
-              onChange={this.onAutoformattingToggle}
-              checked={this.state.autoformat} /> Autoformat
-          </Toolbar>
+        <EditorPane
+          className="splitpane"
+          object={object}
+          payload={payload}
+          onPayloadChanged={this.onPayloadChanged}
+        />
 
-          <JSONEditor
-            className={ cx({ invalid: !object }) }
-            payload={this.state.payload}
-            onPayloadChanged={this.onPayloadChanged} />
-        </div>
-
-        <div className="splitpane">
-          <Viewer
-            object={this.state.object}
-            hash={hashCode(this.state.payload)}
-          />
-        </div>
+        <Viewer
+          className="splitpane"
+          object={object}
+          hash={hashCode(payload)}
+        />
       </div>
     );
   }
---
+++ src/components/EditorPane.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import cx from 'classnames';
+
+import JSONEditor from './JSONEditor';
+import Toolbar from './Toolbar';
+
+import { formattedJSON } from '../utils/json';
+
+export default class EditorPane extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { autoformat: true };
+    this.onAutoformattingToggle = this.onAutoformattingToggle.bind(this);
+  }
+
+  onAutoformattingToggle(event) {
+    let autoformat = event.target.checked;
+    this.setState({ autoformat });
+  }
+
+  render() {
+    let {
+      className,
+      object,
+      payload,
+      onPayloadChanged
+    } = this.props;
+
+    let { autoformat } = this.state;
+
+    if (object && autoformat) {
+      payload = formattedJSON(object);
+    }
+
+    return(
+      <div className={className}>
+
+        <Toolbar>
+          <input
+            className="autoformat"
+            type="checkbox"
+            onChange={this.onAutoformattingToggle}
+            checked={autoformat} /> Autoformat
+        </Toolbar>
+
+        <JSONEditor
+          className={ cx({ invalid: !object }) }
+          payload={payload}
+          onPayloadChanged={onPayloadChanged} />
+
+      </div>
+    );
+  }
+}
