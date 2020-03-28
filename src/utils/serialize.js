2abb534b35f575062fb4df2c21de8cdd9e66e910
Vitalii Elenhaupt <velenhaupt@gmail.com>
2020-03-28 09:51:35 +0200

    Support recursive payloads
    closes #6

--- src/utils/serialize.js
+++ src/utils/serialize.js
@@ -1,20 +1,22 @@
 const TEMP_ID = 'temp-id';
 
-export default function serialize(object) {
+export default function serialize(object, cache = new Map()) {
   if (!object.data) return {};
-  return serializeObject(object.data, object.included || []);
+  return serializeObject(object.data, object.included || [], cache);
 }
 
-function serializeObject(data, included) {
+function serializeObject(data, included, cache) {
   let object = {
     id: data.id,
     tempId: data[TEMP_ID],
-    uuid: `${data.type}-${data.id || data[TEMP_ID]}`,
+    uuid: uuid(data),
     type: data.type,
     attributes: data.attributes || {},
     method: data.method,
     children: []
   };
+
+  cache.set(object.uuid, object);
 
   let { relationships } = data;
 
@@ -22,12 +24,11 @@ function serializeObject(data, included) {
     let relationshipData = relationships[key]["data"];
 
     if (Array.isArray(relationshipData)) {
-      let children = relationshipData.map((relationship) => (
-        serializeRelationship(relationship, included)
-      ));
+      let children = relationshipData
+        .map((relationship) => serializeRelationship(relationship, included, cache));
       object.children.push(...children);
     } else {
-      let child = serializeRelationship(relationshipData, included);
+      let child = serializeRelationship(relationshipData, included, cache);
       object.children.push(child);
     }
   }
@@ -35,9 +36,18 @@ function serializeObject(data, included) {
   return object;
 }
 
-function serializeRelationship(data, included) {
+function serializeRelationship(data, included, cache) {
+  let visited_relation = cache.get(uuid(data));
+  if (visited_relation) {
+    return {
+      ...visited_relation,
+      circular: true,
+      children: []
+    }
+  }
+
   let relationshipData = findInIncluded(data, included) || data;
-  let relationship = serializeObject(relationshipData, included);
+  let relationship = serializeObject(relationshipData, included, cache);
 
   if (!relationship.method) relationship.method = data.method;
 
@@ -54,4 +64,8 @@ function findInIncluded(data, included) {
   }
 
   return null;
+}
+
+function uuid(data) {
+  return `${data.type}-${data.id || data[TEMP_ID]}`;
 }
