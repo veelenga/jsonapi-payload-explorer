016be504d8807b2ea7748a8c2010d7a2d6b96284
Vitalii Elenhaupt <velenhaupt@gmail.com>
2020-12-09 13:29:00 +0200

    Run tests on CI

---
+++ src/__tests__/utils/curl.spec.js
@@ -0,0 +1,24 @@
+import { tryParseCURL } from "../../utils/curl.js"
+
+describe("tryParseCURL", () => {
+  test("it should return null if string is not cURL", () => {
+    expect(tryParseCURL("{}")).toEqual(null)
+  });
+
+  test("it should return null if curl does not have JSON body", () => {
+    let command = `
+      curl -d "@data.txt" -X POST http://localhost:3000/data
+    `
+    expect(tryParseCURL(command)).toEqual(null)
+  });
+
+  test("it should return body if curl have JSON body", () => {
+    let command = `
+      curl -d '{"key1":"value1", "key2":"value2"}'\
+           -H "Content-Type: application/json" \
+           -X POST http://localhost:3000/data
+    `
+    expect(tryParseCURL(command))
+      .toEqual("{\"key1\":\"value1\", \"key2\":\"value2\"}")
+  });
+});
